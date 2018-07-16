8792d75378bd244602edab88c0110bbe9b04b63b
Charlie Jackson <charlie@charliejackson.com>
2018-07-16 15:32:21 +0100

    waitFor actually times out

--- packages/react-dom-model-selectors/src/waitFor.js
+++ packages/react-dom-model-selectors/src/waitFor.js
@@ -1,33 +1,43 @@
 module.exports = (func, timeout = 5000) => {
-    const startTime = Date.now();
+  const startTime = Date.now();
 
-    const attempt = () => {
-      try {
-        const promise = func();
-        return Promise.resolve(promise).then(() => null).catch((e) => e || new Error('Undefined error'));
-      } catch (e) {
-        return Promise.resolve(e);
-      }
+  const attempt = () => {
+    try {
+      const promise = func();
+      return Promise.resolve(promise)
+        .then(() => null)
+        .catch(e => e || new Error('Undefined error'));
+    } catch (e) {
+      return Promise.resolve(e);
     }
+  };
 
-    const loop = () => attempt().then((error) => {
+  let ended = false;
+
+  const loop = () =>
+    attempt().then(error => {
+      if (ended) return Promise.resolve();
       if (!error) return Promise.resolve();
 
-      return new Promise((resolve, reject) => setTimeout(() => {
-        const now = Date.now();
-
-        if (now - startTime > timeout) {
-          const errorMessage = `waitFor exceeded timeout of ${timeout}.`;
-          console.error(errorMessage);
-          console.error(error);
-
-          reject(new Error(`${errorMessage} Check logs above for more info`));
-          return;
-        }
-
-        return loop().then(resolve).catch(reject);
-      }, 300));
+      return new Promise((resolve, reject) =>
+        setTimeout(
+          () =>
+            loop()
+              .then(resolve)
+              .catch(reject),
+          300
+        )
+      );
     });
 
-    return loop();
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      ended = true;
+      reject(new Error(`waitFor exceeded timeout of ${timeout}.`));
+    }, timeout);
+
+    loop()
+      .then(resolve)
+      .catch(reject);
+  });
+};
